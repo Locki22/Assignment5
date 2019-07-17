bdff22d1d1273f5087066932453d26f5bd6dfb07
Locki22 <52662475+Locki22@users.noreply.github.com>
2019-07-17 21:27:07 +0200

    Add files via upload
    SWAT

---
+++ basic_winterwheat/000010001.sol
@@ -0,0 +1,20 @@
+ .sol file Subbasin:1 HRU:1 Luse:AGRL Soil:Braunerde Wed Dec 31 12:33:10 2003 AVSWAT2000 - SWAT interface MDL
+ Soil Name: Braunerde
+ Soil Hydrologic Group: A
+ Maximum rooting depth(m) : 2000.00
+ Porosity fraction from which anions are excluded: 0.500
+ Crack volume potential of soil: 0.000
+ Texture 1                : 
+ Depth                [mm]:      300.00     1700.00
+ Bulk Density Moist [g/cc]:        1.5        1.5
+ Ave. AW Incl. Rock Frag  :        0.20        0.20
+ Ksat. (est.)      [mm/hr]:       30.00       30.00
+ Organic Carbon [weight %]:        2.00        0.25
+ Clay           [weight %]:        5.00        5.00
+ Silt           [weight %]:       15.00       15.00
+ Sand           [weight %]:       80.00       80.00
+ Rock Fragments   [vol. %]:        0.00        0.00
+ Soil Albedo (Moist)      :        0.25        0.25
+ Erosion K                :        0.20        0.20
+ Salinity (EC, Form 5)    :        0.00        0.00
+                              
